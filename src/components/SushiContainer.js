22e9f7af095202d6fb5a822042f649eb89a49170
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-16 09:06:52 -0400

    show list, more sushi button works

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import Sushi from "./Sushi"
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({sushiList}) {
+
+  const [count, setcount] = useState(4)
+
+  console.log("passed Array:", sushiList)
+
+  const filterSushi = sushiList.filter((item)=> (item.id > count-4) && (item.id <= count))
+  
+  console.log(filterSushi)
+
+  function showMoreSushi(){
+    console.log("I've been clicked")
+    setcount(()=> count + 4)
+  }
+
+
+
+
+  const showSushi = filterSushi.map((item)=> {
+    return <Sushi 
+    key= {item.id}
+    id = {item.id}
+    name= {item.name}
+    img_url= {item.img_url}
+    price= {item.price}
+    />
+  })
+
+
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {showSushi}
+      <MoreButton showMoreSushi={showMoreSushi} />
     </div>
   );
 }
